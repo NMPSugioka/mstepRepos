2e4313088eda42fb5ca966ae904f1e3dd1b607b1
NMPSugioka <sugisizu@hotmail.com>
2019-02-20 17:59:38 +0900

    プロジェクト ファイルを追加します。

---
+++ FunctionApp6/Function2.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp6
+{
+    public static class Function2
+    {
+        [FunctionName("Function2")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "databaseName",
+            collectionName: "collectionName",
+            ConnectionStringSetting = "",
+            LeaseCollectionName = "leases")]IReadOnlyList<Document> input, ILogger log)
+        {
+            if (input != null && input.Count > 0)
+            {
+                log.LogInformation("Documents modified " + input.Count);
+                log.LogInformation("First document Id " + input[0].Id);
+            }
+        }
+    }
+}
